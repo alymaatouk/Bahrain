a5357dc456d38a15a818f02c87996965d2bb6b81
Ali Maatouk <115188345+alymaatouk@users.noreply.github.com>
2023-03-02 11:04:58 +0200

    Update streamlit_students_dashboard.py

--- streamlit_students_dashboard.py
+++ streamlit_students_dashboard.py
@@ -28,6 +28,16 @@ with lastt_co :
     st.title("البحرين")
     st.write('## الرجاء اختيار موضوع الاستبيان عبر الضغط على الأزرار ادناه ')
 
+    
+from bokeh.models.widgets import Div
+import streamlit as st
+
+if st.button('Go to Streamlit'):
+    js = "window.open('https://www.streamlit.io/')"  # New tab or window
+    js = "window.location.href = 'https://www.streamlit.io/'"  # Current tab
+    html = '<img src onerror="{}">'.format(js)
+    div = Div(text=html)
+    st.bokeh_chart(div)
 #Welcome Message 
 
 
