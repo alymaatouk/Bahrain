7c2a294e0c15305821573b3b8cee2d1d0c3a9944
Ali Maatouk <115188345+alymaatouk@users.noreply.github.com>
2023-03-02 10:50:19 +0200

    Update streamlit_students_dashboard.py

--- streamlit_students_dashboard.py
+++ streamlit_students_dashboard.py
@@ -15,7 +15,7 @@ left_co, cent_co,last_co = st.columns(3)
 with cent_co:
     st.image("https://i2.wp.com/ummah-futures.net/wp-content/uploads/2019/12/%D8%A7%D9%84%D9%84%D8%AC%D9%86%D8%A9-%D8%A7%D9%84%D8%A7%D9%82%D8%AA%D8%B5%D8%A7%D8%AF%D9%8A%D8%A9-%D9%88%D8%A7%D9%84%D8%A7%D8%AC%D8%AA%D9%85%D8%A7%D8%B9%D9%8A%D8%A9-%D9%84%D8%BA%D8%B1%D8%A8%D9%8A-%D8%A2%D8%B3%D9%8A%D8%A7-1.jpg?w=500&ssl=1")
     
-left_col, center_co, last = st.columns([1,4,1])
+left_col, center_co = st.columns([1,4])
 with center_co:
     st.title(""" 17 - "اســتـبـيـان "المجتمع العربي: مجموعة الاحصاءات والمؤشرات الاجتماعية""")
 
